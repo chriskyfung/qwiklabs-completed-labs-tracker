83e8f54e85f7c971e25b6e603e28e32728075565
Chris K.Y. FUNG <chris.ky.fung@connect.polyu.hk>
2019-09-01 11:17:50 +0800

    v0.2

--- userscript.js
+++ userscript.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Qwiklabs Complete Indicator
 // @namespace    qwiklabs_web
-// @version      0.1
+// @version      0.2
 // @description  Label completed quests and labs on the Catalog page(s) on Qwiklabs (https://www.qwiklabs.com/catalog)
 // @author       chriskyfung
 // @match        https://www.qwiklabs.com/catalog*
 // @match        https://google.qwiklabs.com/catalog*
+// @match        https://www.qwiklabs.com/focuses/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var t = document.querySelectorAll('.catalog-item__title');
+    
     // REPLACE WITH THE YOUR COMPLETED QUEST TITLES IN THE ARRAY BELOW
     var completed_quests = [ "Application Development - Python",
                              "Data Engineering",
@@ -19,7 +20,12 @@
     // REPLACE WITH THE YOUR COMPLETED LAB TITLES IN THE ARRAY BELOW
     var completed_labs = [ "Getting Started with Cloud Shell & gcloud",
                            "A Tour of Qwiklabs and the Google Cloud Platform",
-                         ]
+                         ];
+                         
+    var t = document.querySelectorAll('.catalog-item__title');
+    if (t.length ==0) {
+        t = [document.querySelector("div.header__title > h1")];
+    };
 
     var i;
     for (i=0; i<t.length;i++){
