b9fe8fdce987a9babd615529484369d7d0fbfd30
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2021-10-13 17:15:10 +0800

    🛠 Setup ESLint and Prettier for Dev Environment

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  printWidth: 80,
+};
